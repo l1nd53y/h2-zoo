96e50e5242740bc96681c7872a3e1d7fdec1a917
l1nd53y <lndsadams@gmail.com>
2021-06-14 14:20:44 -0500

    Additions

--- src/main/java/com/example/h2zoo/H2ZooApplication.java
+++ src/main/java/com/example/h2zoo/H2ZooApplication.java
@@ -19,7 +19,7 @@ public class H2ZooApplication {
 
 		ConfigurableApplicationContext configurableApplicationContext =
 			SpringApplication.run(H2ZooApplication.class, args);
-		
+
 		ReptileRepository reptileRepository = configurableApplicationContext.getBean(ReptileRepository.class);
 		Reptile kingCobra = new Reptile("Slither", "Snake", "Black & Yellow", "Carnivore");
 		Reptile komodoDragon = new Reptile("Dennis", "Lizard", "Brown", "Carnivore");
@@ -29,13 +29,15 @@ public class H2ZooApplication {
 		Dog miniSchnauzer = new Dog("Neville", "Miniature Schnauzer", "Grey", "Marshmallows");
 		Dog havanese = new Dog("Nox", "Havanese", "Black", "Cookies");
 		Dog maltipoo = new Dog("Jupiter", "Maltipoo", "White", "Stars");
-		dogRepository.saveAll(List.of(miniSchnauzer, havanese, maltipoo));
+		Dog literary = new Dog("Clifford", "Big", "Red", "Bowser Bites");
+		dogRepository.saveAll(List.of(miniSchnauzer, havanese, maltipoo, literary));
 
 		CatRepository catRepository = configurableApplicationContext.getBean(CatRepository.class);
 		Cat persian = new Cat("Hannah", "Persian", "White & Grey", "Poptarts");
-		Cat cartoon = new Cat("Figaro", "Unknown", "Black & White", "Goldfish");
-		Cat aristocat = new Cat("Thomas O'Malley", "Alley", "Orange", "Anything");
-		catRepository.saveAll(List.of(persian, cartoon, aristocat));
+		Cat cartoon = new Cat("Figaro", "Tuxedo", "Black & White", "Goldfish");
+		Cat aristocat = new Cat("Thomas O'Malley", "Alley", "Orange", "Garbage");
+		Cat comic = new Cat("Garfield", "Fat", "Orange", "Lasagna");
+		catRepository.saveAll(List.of(persian, cartoon, aristocat, comic));
 	}
 
 }
